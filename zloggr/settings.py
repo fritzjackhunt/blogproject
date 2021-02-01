92b489a0ce3b6c380958dd538d4742fca60adee2
fritzjackhunt <essiet.aniekan@yahoo.com>
2021-02-01 11:58:37 +0300

    Requirements for heroku

--- zloggr/settings.py
+++ zloggr/settings.py
@@ -20,10 +20,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yo5ve94x50js!0y4(96-ng1!jdv-=%x(va36hf)mx-d4^sw8b('
+#SECRET_KEY = 'yo5ve94x50js!0y4(96-ng1!jdv-=%x(va36hf)mx-d4^sw8b('
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'yo5ve94x50js!0y4(96-ng1!jdv-=%x(va36hf)mx-d4^sw8b(')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -48,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'zloggr.urls'
@@ -123,3 +129,7 @@ STATIC_ROOT = '/blogproject/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
