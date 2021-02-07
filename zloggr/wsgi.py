daa091481edfbe71b2fde993e0d17e147f1084d8
fritzjackhunt <essiet.aniekan@yahoo.com>
2021-02-07 20:43:39 +0300

    Modified wsgi.py file

--- zloggr/wsgi.py
+++ zloggr/wsgi.py
@@ -9,7 +9,20 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+import django
+from django.core.handlers.wsgi import WSGIHandler
+
+
+def get_wsgi_application():
+    """
+    The public interface to Django's WSGI support. Return a WSGI callable.
+
+    Avoids making django.core.handlers.WSGIHandler a public API, in case the
+    internal WSGI implementation changes or moves in the future.
+    """
+    django.setup(set_prefix=False)
+    return WSGIHandler()
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zloggr.settings')
 
