bc6d2ed2232f4ec57ba5a40be3be6c48fdabeb90
fritzjackhunt <essiet.aniekan@yahoo.com>
2021-02-07 21:03:17 +0300

    Made changes wsgi.py

--- zloggr/wsgi.py
+++ zloggr/wsgi.py
@@ -7,22 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
-import django
-from django.core.handlers.wsgi import WSGIHandler
+# add the hellodjango project path into the sys.path
+sys.path.append('C:/Users/ANIEKAN/jackfritz/blogproject')
 
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('C:/Python38/Lib/site-packages')
 
-def get_wsgi_application():
-    """
-    The public interface to Django's WSGI support. Return a WSGI callable.
-
-    Avoids making django.core.handlers.WSGIHandler a public API, in case the
-    internal WSGI implementation changes or moves in the future.
-    """
-    django.setup(set_prefix=False)
-    return WSGIHandler()
-
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zloggr.settings')
 
